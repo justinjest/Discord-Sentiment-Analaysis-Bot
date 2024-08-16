d673e4b68090467d792fba2830f264a7d85cf47d
[Jessica Cayaban] <[jest.pringle@gmail.com]>
2024-08-17 08:21:26 +0900

    Basic chat functionality, responds with 'hello' when a user sends hello

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+import secret
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -17,4 +18,4 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-client.run(token)
+client.run(secret.token)
