caa7f34753265dc597fd74043709aa1dd00e8979
[Jessica Cayaban] <[jest.pringle@gmail.com]>
2024-08-18 13:52:33 +0900

    Added basic OPENAI sentiment analysis, worked on setting up some async for loops which are surprisingly confusing, and finally got to the point where my discord API key does not have sufficent permissions to make the changes I need in order to change channel permissions.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import discord
-import openai
+from openai import OpenAI
 import secret
 import threading
 
@@ -9,10 +9,9 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
-# Open Ai API setups
-client = openai(
-    organization = secret.open_ai_org,
-    project = secret.project_id
+# Open Ai API setups    
+openai_client = OpenAI(
+    api_key = secret.project_id
 )
 
 # Placeholder to prove that python scripts run within discord
@@ -23,16 +22,28 @@ def reverse_string(input):
         return("Error with string, unable to reverse")
     
 def get_openai_response(input:str) -> float:
-    # TODO access openai to call the sentiment analysis function on input
-    return
+    # TODO improve prompt to something slightly cleaner
+    completion = openai_client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "user",
+                "content": "You will rank all messages you receive with a score from -1 to 1. You will rank helpful, or happy messages with a score of 1. You will rank angry, frustrated, or rude messages with a score of -1. You will rank it 0 if a message is neither. You will rank messages between 1 and 0 if they are somewhat helpful, you will rank a message between 0 and -1 if it is somewhat angry. You will provide no response except for a single float.",
+            },
+        ],
+    )
+    return (completion.choices[0].message.content)
 
-def activate_timeout():
+async def activate_timeout(channel_name):
     # TODO activate slow mode in discord
-    return
+    await channel_name.edit(slowmode_delay=60)
+    await channel_name.send("Slowmode activated due to hot heads")
 
-def end_timeout():
+async def end_timeout(channel_name):
     # TODO turn off slow mode in discord
-    return
+    await channel_name.edit(slowmode_delay=0)
+    await channel_name.send("Slowmode turned off")
+    
 
 def mood_decay(current_anger: float) -> float:
     # TODO multi threaded argument that decays the anger level over time
@@ -48,13 +59,16 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.author == client.user:
+        await end_timeout(message.channel)
         return
     else:
-        await current_anger += get_openai_response(message.content)
+        await message.channel.send(get_openai_response(message.content))
+        await activate_timeout(message.channel)
 
 # Constantly running in one thread
 def mood_management(current_anger):
     if current_anger > 5:
+        mood_decay(current_anger)
         activate_timeout
         mood_management(current_anger)
     elif current_anger <= 0:
